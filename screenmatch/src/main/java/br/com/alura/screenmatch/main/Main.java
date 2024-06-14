c831ea0523fdc6333419acf47efeb21a6151c5b0
Weslley Evangelista <weslleylello@gmail.com>
2024-06-14 17:05:10 -0300

    1- Coding refactor Main class (Refatoração do código na classe Main)
    2- Added a method to list the searched series
    (Adicionado um método para listar as séries já buscadas)

--- screenmatch/src/main/java/br/com/alura/screenmatch/main/Main.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/main/Main.java
@@ -7,9 +7,7 @@ import br.com.alura.screenmatch.model.SeriesEpisodes;
 import br.com.alura.screenmatch.service.ApiConsumption;
 import br.com.alura.screenmatch.service.DataConversion;
 
-import java.time.LocalDate;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     private Scanner scanner = new Scanner(System.in);
@@ -17,94 +15,70 @@ public class Main {
     private DataConversion conversion = new DataConversion();
     private final String URL = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=ddf8730b";
+    private List<SeriesData> seriesData = new ArrayList<>();
 
     public void displayMenu() {
-        System.out.println("Type the movie/series you wish to search: ");
-        var titleName = scanner.nextLine();
+        var option = -1;
 
-        var json = consumption.getData(URL + titleName.replace(" ", "+") + API_KEY);
-        SeriesData data = conversion.getData(json, SeriesData.class);
+        while(option != 0) {
+            var menu = """
+                    1 - Search for series
+                    2 - Search for series episodes
+                    3 - List searched series
+                                   \s
+                    0 - Exit
+                   \s""";
 
+            System.out.println(menu);
+            option = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1:
+                    searchSeriesWeb();
+                    break;
+                case 2:
+                    searchEpisodeBySeries();
+                    break;
+                case 3:
+                    listSearchedSeries();
+                    break;
+                case 0:
+                    System.out.println("Exiting program...");
+                    break;
+                default:
+                    System.out.println("Invalid option!");
+            }
+        }
+    }
+
+    private void searchSeriesWeb() {
+        SeriesData data = getSeriesData();
+        seriesData.add(data);
         System.out.println(data);
+    }
 
+    private SeriesData getSeriesData() {
+        System.out.println("Insert the series name to search: ");
+        var seriesName = scanner.nextLine();
+        var json = consumption.getData(URL + seriesName.replace(" ", "+") + API_KEY);
+        SeriesData data = conversion.getData(json, SeriesData.class);
+        return data;
+    }
+
+    private void searchEpisodeBySeries() {
+        SeriesData seriesData = getSeriesData();
         List<SeasonData> seasons = new ArrayList<>();
 
-		for (int i = 1; i < data.totalSeasons() ; i++) {
-			json = consumption.getData(URL + titleName.replace(" ", "+") + "&season=" + i + API_KEY);
-			SeasonData seasonData = conversion.getData(json, SeasonData.class);
-
-			seasons.add(seasonData);
-		}
-
-		seasons.forEach(System.out::println);
-
-        seasons.forEach(s -> s.episodes().forEach(e -> System.out.println(e.title())));
-
-        List<SeriesEpisodeData> episodesData = seasons.stream()
-                .flatMap(s -> s.episodes().stream())
-                .collect(Collectors.toList());
-
-        System.out.println("Top 10 episodes: ");
-        episodesData.stream()
-                .filter(e -> !e.rating().equalsIgnoreCase("N/A"))
-                .peek(e -> System.out.println("First filter(N/A) " + e))
-                .sorted(Comparator.comparing(SeriesEpisodeData::rating).reversed())
-                .peek(e -> System.out.println("Ordered " + e))
-                .limit(10)
-                .peek(e -> System.out.println("Limit " + e))
-                .map(e -> e.title().toUpperCase())
-                .peek(e -> System.out.println("Map " + e))
-                .forEach(System.out::println);
-
-        List<SeriesEpisodes> episodes = seasons.stream()
-                .flatMap(s -> s.episodes().stream().map(d -> new SeriesEpisodes(s.number(), d)))
-                .collect(Collectors.toList());
-
-        episodes.forEach(System.out::println);
-
-        System.out.println("Enter an title excerpt of the episode of the series that you are looking for: ");
-        var titlePart = scanner.nextLine();
-
-        Optional<SeriesEpisodes> searchedEpisode = episodes.stream()
-                .filter(e -> e.getTitle().toUpperCase().contains(titlePart.toUpperCase()))
-                .findFirst();
-
-        if (searchedEpisode.isPresent()) {
-            System.out.println("Episode found!");
-            System.out.println("Season: " + searchedEpisode.get().getSeasonNumber());
-        } else {
-            System.out.println("Episode not found!");
+        for (int i = 1; i <= seriesData.totalSeasons(); i++) {
+            var json = consumption.getData(URL + seriesData.title().replace(" ", "+") + API_KEY);
+            SeasonData seasonData = conversion.getData(json, SeasonData.class);
+            seasons.add(seasonData);
         }
+        seasons.forEach(System.out::println);
+    }
 
-        System.out.println("Search episodes by year: ");
-        var year = scanner.nextInt();
-        scanner.nextLine();
-
-        LocalDate searchDate = LocalDate.of(year,1,1);
-
-        episodes.stream()
-                .filter(e -> e.getReleaseDate() != null && e.getReleaseDate().isAfter(searchDate))
-                .forEach(e -> System.out.println(
-                        "Season: " + e.getSeasonNumber() +
-                                " | Episode: " + e.getTitle() +
-                                " | Release Date: " + e.getReleaseDate()
-                ));
-
-        Map<Integer, Double> ratingBySeason = episodes.stream()
-                .filter(e -> e.getRating() > 0.0)
-                .collect(Collectors.groupingBy(
-                                SeriesEpisodes::getSeasonNumber,
-                                Collectors.averagingDouble(SeriesEpisodes::getRating)));
-
-        System.out.println(ratingBySeason);
-
-        DoubleSummaryStatistics statistics = episodes.stream()
-                .filter(e -> e.getRating() > 0.0)
-                .collect(Collectors.summarizingDouble(SeriesEpisodes::getRating));
-
-        System.out.println("Average rating: " + statistics.getAverage());
-        System.out.println("Lowest rating: " + statistics.getMin());
-        System.out.println("Highest rating: " + statistics.getMax());
-        System.out.println("Total episodes rated: " + statistics.getCount());
+    private void listSearchedSeries() {
+        seriesData.forEach(System.out::println);
     }
 }
